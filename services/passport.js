9d0fa28fea922659b9bceba987b0af71ef3f40cb
Dmitrij Feller <ing.d.feller@gmail.com>
2018-07-21 21:08:15 +0200

    add cookie-session and improve auth flow

--- services/passport.js
+++ services/passport.js
@@ -5,10 +5,26 @@ const mongoose = require('mongoose');
 
 const User = mongoose.model('users');
 
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then(user => done(null, user));
+});
+
 passport.use(new GoogleStrategy({
   clientID: keys.googleClientID,
   clientSecret: keys.googleClientSecret,
   callbackURL: '/auth/google/callback',
 }, (accessToken, refreshToken, profile, done) => {
-  new User({ googleID: profile.id }).save();
+  User.findOne({ googleID: profile.id }).then((existingUser) => {
+    if (existingUser) {
+    // we already have a record
+      done(null, existingUser);
+    } else {
+    // we don't have a user with this ID
+      new User({ googleID: profile.id }).save().then(user => done(null, user));
+    }
+  });
 }));
