2e464f53cc6d71804d51970603eb542dd4be7b1d
Dmitrij Feller <ing.d.feller@gmail.com>
2018-07-21 21:07:16 +0200

    add keys-example.js file

---
+++ config/keys-example.js
@@ -0,0 +1,6 @@
+module.exports = {
+  googleClientID: '29241****************************************.apps.googleusercontent.com',
+  googleClientSecret: 'exampleSecret',
+  mongoDevURI: 'mongodb://<db-user>:<db-password>@******.mlab.com:47001/yourApp',
+  cookieKey: 'randomNumbers',
+};
