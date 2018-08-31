32e861c2304925f5aa916a340c553ac521a79d7e
Dmitrij Feller <ing.d.feller@gmail.com>
2018-08-31 13:28:05 +0200

    adjust Header based on the auth status

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,19 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class Header extends Component{
+	renderContent() {
+		switch (this.props.auth) {
+			case null:
+				return;
+			case false:
+				return (
+					<li><a href="/auth/google">Login with Google</a></li>
+				);
+			default:
+				return <li><a href="#">Logout</a></li>
+		}
+	}
   render(){
     return(
       <nav>
@@ -9,9 +22,7 @@ class Header extends Component{
             Maild
           </a>
           <ul className="right">
-            <li>
-              <a> Login With Google</a>
-            </li>
+						{this.renderContent()}
           </ul>
         </div>
       </nav>
@@ -19,5 +30,7 @@ class Header extends Component{
   }
 }
 
-
-export default Header;
+function mapStateToProps({ auth }) {
+	return { auth };
+}
+export default connect(mapStateToProps)(Header);
